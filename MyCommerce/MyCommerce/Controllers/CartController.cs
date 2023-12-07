402148e5060e568a0f0959ad23f2eee39d2c2091
khuong do <dmkhuongc7htk2019@gmail.com>
2023-12-07 17:18:27 +0700

    first commit

---
+++ MyCommerce/MyCommerce/Controllers/CartController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCommerce.Data;
+using MyCommerce.Models;
+
+namespace MyCommerce.Controllers
+{
+    public class CartController : Controller
+    {
+        private MyeStoreContext _ctx;
+
+        public CartController(MyeStoreContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public List<CartItem> Cart
+        {
+            get
+            {
+                var carts = HttpContext.Session.Get<List<CartItem>>("CART");
+                if (carts == null)
+                {
+                    carts = new List<CartItem>();
+                }
+                return carts;
+            }
+        }
+        public IActionResult AddToCart(int id, int qty = 1)
+        {
+            var gioHang = Cart;
+
+            //kiểm tra id (MaHH) truyền qua đã nằm trong giỏ hàng hay chưa
+            var item = gioHang.SingleOrDefault(p => p.MaHh == id);
+            if (item != null) //đã có
+            {
+                item.SoLuong += qty;
+            }
+            else
+            {
+                var hangHoa = _ctx.HangHoas.SingleOrDefault(p => p.MaHh == id);
+                if (hangHoa == null)//id không có trong Database
+                {
+                    return RedirectToAction("Index", "HangHoas");
+                }
+                item = new CartItem
+                {
+                    MaHh = id,
+                    SoLuong = qty,
+                    TenHh = hangHoa.TenHh,
+                    Hinh = hangHoa.Hinh,
+                    DonGia = hangHoa.DonGia.Value
+                };
+                //thêm vào giỏ hàng
+                gioHang.Add(item);
+            }
+            //cập nhật session
+            HttpContext.Session.Set("CART", gioHang);
+            return RedirectToAction("Index");
+        }
+        public IActionResult RemoveCart(int id)
+        {
+            var gioHang = Cart;
+            var item = gioHang.SingleOrDefault(p => p.MaHh == id);
+            if (item != null)
+            {
+                gioHang.Remove(item);
+                HttpContext.Session.Set("CART", gioHang);
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyCommerce/MyCommerce/Controllers/KhachHangController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using MyCommerce.Data;
+using MyCommerce.Models;
+using System.Security.Claims;
+
+namespace MyCommerce.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        private readonly MyeStoreContext _ctx;
+
+        public KhachHangController(MyeStoreContext ctx)
+        {
+            _ctx = ctx;
+        }
+        [HttpPost]
+        public IActionResult DangKy(RegisterVM model, IFormFile FileHinh)
+        {
+            try
+            {
+                var khachHang = new KhachHang
+                {
+                    MaKh = model.MaKh,
+                    HoTen = model.HoTen,
+                    NgaySinh = model.NgaySinh,
+                    DiaChi = model.DiaChi,
+                    GioiTinh = model.GioiTinh,
+                    DienThoai = model.DienThoai,
+                    Email = model.Email,
+                    Hinh = MyTool.UploadFileToFolder(FileHinh, "KhachHang"),
+
+                    HieuLuc = true, //false + gửi mail active tài khoản
+                    RandomKey = MyTool.GetRandom()
+                };
+                khachHang.MatKhau = model.MatKhau.ToMd5Hash(khachHang.RandomKey);
+                _ctx.Add(khachHang);
+                _ctx.SaveChanges();
+                return RedirectToAction("DangNhap");
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> DangNhap(LoginVM model)
+        {
+            var kh = _ctx.KhachHangs.SingleOrDefault(p => p.MaKh == model.UserName);
+            if (kh == null)
+            {
+                ViewBag.ThongBaoLoi = "User này không tồn tại";
+                return View();
+            }
+
+            if (kh.MatKhau != model.Password.ToMd5Hash(kh.RandomKey))
+            {
+                ViewBag.ThongBaoLoi = "Đăng nhập không thành công";
+                return View();
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, kh.Email),
+                new Claim("FullName", kh.HoTen),
+
+                new Claim(ClaimTypes.Role, "Administrator"),//động
+            };
+
+            var claimsIdentity = new ClaimsIdentity(
+            claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var claimPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+            await HttpContext.SignInAsync(claimPrincipal);
+
+            return Redirect("/");
+        }
+
+    }
+
+}
